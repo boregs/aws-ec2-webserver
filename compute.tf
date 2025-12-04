0922fd2aa0cbcce60f1cc45a51ed0c1018ecfd30
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-12-04 20:51:25 -0300

    fixing first commit

--- compute.tf
+++ compute.tf
@@ -7,8 +7,8 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
     most_recent = true # Busca a mais recente
 
-
     
+
     filter {
         name = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
