464cf6f95af425d1c26ddb5f8e110944a09592f4
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-12-04 21:16:22 -0300

    minor changes

--- compute.tf
+++ compute.tf
@@ -1,26 +1,29 @@
-# Declara o provedor cloud que sera usado e sua regiao
+# Declares the AWS provider
 provider "aws" {
     region = "us-east-2"
 }
 
-# Declara o tipo de dados que serao usados, nesse caso vai ser a instancia EC2 com servidor Ubuntu
+# Declares the data resource to search for the latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
-    most_recent = true # Busca a mais recente
+    most_recent = true # searches for the most recent AMI available
 
-
+    vpc_security_group_ids = [aws_security_group.aws_sg.id]
 
     filter {
         name = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
     }
 
-    owners = ["099720109477"] # ID da distribuidora oficial das imagens Ubunto na AWS
+    owners = ["099720109477"] # The ID of the official AWS Ubuntu image provider
 }
  
-# Declara o recurso que sera criado, nesse caso uma instancia EC2 com o AMI do Ubuntu
+# Declares the EC2 instance resource
 resource "aws_instance" "aws_server" {
     ami = data.aws_ami.ubuntu.id
     instance_type = "t3.micro"
+
+    # Adds to the instance the security group created in aws_security_group.tf
+    vpc_security_group_ids = [aws_security_group.aws_sg.id]
 
     tags = {
         Name = "AWS Project Instance"
