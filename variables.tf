63b4f6d9b8b1b5b8ece4c41627b9042ee43fb207
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-12-05 23:43:25 -0300

    minor changes

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
-variable "rds_password"{
+variable "rdsPassword"{
     description = "The RDS database password"
     type = string
     sensitive = true
+}
+
+variable "namePrefix" {
+    description = "Prefix for naming AWS resources"
+    type        = string
+    default     = "AWSProject"
 }
