e65af4b50065d1d585a70af1bc5b79dacdcacfc9
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-12-07 09:54:00 -0300

    minor changes

---
+++ modules/app-services/variables.tf
@@ -0,0 +1,17 @@
+variable "rdsPassword"{
+    description = "The RDS database password"
+    type = string
+    sensitive = true
+}
+
+variable "namePrefix" {
+    description = "Prefix for naming AWS resources"
+    type        = string
+    default     = "AWSProject"
+}
+
+variable "region" {
+    description = "AWS region to deploy the resources"
+    type = string
+    default = "us-east-2"
+}
