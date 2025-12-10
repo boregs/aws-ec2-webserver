ba68163e77faf413332b9662531b9cd506f00cc5
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-12-10 08:02:40 -0300

    minor changes

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+    description = "AWS region to deploy the resources"
+    type = string
+    default = "us-east-2"
+}
+
 variable "rdsPassword"{
     description = "The RDS database password"
     type = string
